4085197674f4949ed9c29f2d0d39c82193668bcd
Pedro Rey Anca <pedroreya@gmail.com>
2020-07-12 16:39:43 +0200

    Fixed menu not working properly

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import telegram
 from telegram.ext import Updater  
 from telegram.ext import CommandHandler
-from telegram.ext import CallbackQueryHandler
-from telegram import InlineKeyboardButton
-from telegram import InlineKeyboardMarkup
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+from telegram import KeyboardButton
+from telegram import ReplyKeyboardMarkup
 from config.telegram import *
 from config.language import *
 from config.gpio import *
@@ -22,7 +23,7 @@ bot = telegram.Bot(token=token)
 updater = Updater(bot.token, use_context=True)
 
 def doorButton():
-    #print("Btn")
+    # print("Btn")
     GPIO.output(gpioPin, GPIO.LOW)
     sleep(0.5)
     GPIO.output(gpioPin, GPIO.HIGH)
@@ -48,10 +49,10 @@ def logCommand(fromChat, cmd, destinationChatId=logChannelId):
     return out
 
 def sendMenu(destinationChatId):
-    keyboard = [[InlineKeyboardButton(_('Open'), callback_data='mOpen')],
-                [InlineKeyboardButton(_('Toggle'), callback_data='mToggle')]]
-    keyboardObj = InlineKeyboardMarkup(keyboard)
-    bot.sendMessage(_("What do you want to do?"), chat_id=destinationChatId, reply_markup=keyboardObj)
+    keyboard = [[KeyboardButton(_('Open'))],
+                [KeyboardButton(_('Toggle'))]]
+    keyboardObj = ReplyKeyboardMarkup(keyboard)
+    bot.sendMessage(chat_id=destinationChatId, text=_("I'm giving you this beautiful menu without any additional cost, you can use it or you can go crazy and use commands; you decide."), reply_markup=keyboardObj)
 
 def sendFuckOff(destinationChatId):
     bot.sendMessage(chat_id=destinationChatId, text=_("I said you this was a private bot, so don't use it!"))
@@ -59,30 +60,28 @@ def sendFuckOff(destinationChatId):
 def start(update, context):
     if logCommand(update.effective_chat, "/start"):
         context.bot.sendMessage(chat_id=update.effective_chat.id, text=_("Hi, I'm Morfeo. Send /help to see avaliable commands"))
-        sendMenu(update)
+        sendMenu(update.effective_chat.id)
 
 def help(update, context):
     if logCommand(update.effective_chat, "/help"):
         context.bot.sendMessage(chat_id=update.effective_chat.id, text=_("This is a list of avaliable commands:\n/open - Opens the door and closes it automatically after 1 minute since you sent the command(It'll do the opposite thing if it's already open)\n/toggle - Just opens/closes the door depending on it's actual state(presses the button one time and forgets about everything).\n/start - The command executed the first time you use the bot.\n/help - this command"))
-        sendMenu(update)
+
 def open(update, context):
     if logCommand(update.effective_chat, "/open"): 
         context.bot.sendMessage(chat_id=update.effective_chat.id, text=_("Opening door... It'll close in 60s"))
         openDoor()
-        sendMenu(update.effective_chat.id)
 
 def toggle(update, context):
     if logCommand(update.effective_chat, "/toggle"): 
         context.bot.sendMessage(chat_id=update.effective_chat.id, text=_("Opening, or closing ;), door..."))
         doorButton()
-        sendMenu(update)
 
 startHandler = CommandHandler('start', start)  
 helpHandler = CommandHandler('help', help)
 openHandler = CommandHandler('open', open)  
 toggleHandler = CommandHandler('toggle', toggle)
-btnOpenHandler = CallbackQueryHandler(open, pattern='mOpen')
-btnToggleHandler = CallbackQueryHandler(toggle, pattern='mToggle')
+btnOpenHandler = MessageHandler(Filters.regex(r"^"+_('Open')+"$"), open)
+btnToggleHandler = MessageHandler(Filters.regex(r"^"+_('Toggle')+"$"), toggle)
 
 dispatcher = updater.dispatcher
 
