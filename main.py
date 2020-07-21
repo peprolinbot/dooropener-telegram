f6d2a4fc79a8eee318aa759df0ed519eac78c8b2
Pedro Rey Anca <pedroreya@gmail.com>
2020-07-21 17:40:53 +0200

    Added comments

--- main.py
+++ main.py
@@ -13,59 +13,62 @@ from config.variables import *
 from time import sleep
 from pydub import AudioSegment
 
+#Loads traductions
 import gettext
 l = gettext.translation('base', localedir='locales', languages=[lang])
 l.install()
 _ = l.gettext
 
+#Initializes pygame
 import pygame
 pygame.init()
 
-import RPi.GPIO as GPIO
+#Sets up piCamera
 from picamera import PiCamera
-
 camera = PiCamera()
 
+#Sets up gpio
+import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(gpioPin, GPIO.OUT)
 
+#Sets up the telegram bot
 bot = telegram.Bot(token=token)  
 updater = Updater(bot.token, use_context=True)
 
-def doorButton():
+def doorButton(): #Presses the door button
     # print("Btn")
     GPIO.output(gpioPin, GPIO.LOW)
     sleep(btnPressTime)
     GPIO.output(gpioPin, GPIO.HIGH)
 
-def openDoor():
+def openDoor(): #Opens door and closes it after specified time
     doorButton()
     sleep(waitToCloseTime)
     doorButton()
 
-def playFile(file_path):
+def playFile(file_path): #Plays the audio file specified
     pygame.mixer.music.load(file_path)
     pygame.mixer.music.set_volume(0.7) 
     pygame.mixer.music.play()
     pygame.event.wait()
-    print("Playing: " + file_path)
 
-def takePhoto(photoPath="doorPhoto.jpg"):
+def takePhoto(photoPath="doorPhoto.jpg"): #Takes a photo to the specified path
     # print("Photo")
     camera.capture(photoPath)
 
-def sendPhoto(destinationChatId, photoPath="doorPhoto.jpg"):
+def sendPhoto(destinationChatId, photoPath="doorPhoto.jpg"): #Sends the phot specified to the chatId specified
     bot.send_photo(chat_id=destinationChatId, photo=open(photoPath, 'rb'))
 
-def checkKey(checkForUserId, checkInchatId=keyChannelId):
+def checkKey(checkForUserId, checkInchatId=keyChannelId): #Checks if specified chatId is in the key channel, or other channel/group specified
     try:
         bot.get_chat_member(checkInchatId, checkForUserId)
     except:
         return False
     return True
 
-def logCommand(fromChat, cmd, destinationChatId=logChannelId):
+def logCommand(fromChat, cmd, destinationChatId=logChannelId): #Logs to the log channel the cmd argument and the details of the fromChat
     if not cmd == "/photo":
         takePhoto()
     sendPhoto(destinationChatId)
@@ -76,58 +79,59 @@ def logCommand(fromChat, cmd, destinationChatId=logChannelId):
         bot.sendMessage(chat_id=destinationChatId, text=cmd + _(": Type: ")+ str(fromChat.type)+ _(", First name: ")+ str(fromChat.first_name) +_(", Last name: ") + str(fromChat.last_name) + _(", Username: ") + str(fromChat.username) + _(", Title: ") + str(fromChat.title) + _(", Description: ") + str(fromChat.description) + _(", chatId: ") + str(fromChat.id) + _("chatIdNotInChannel"))
     return out
 
-def sendMenu(destinationChatId):
+def sendMenu(destinationChatId): #Sends an in-keyboard menu to the specified destinationChatId
     keyboard = [[KeyboardButton(_("open"))],
                 [KeyboardButton(_("toggle"))],
                 [KeyboardButton(_("photo"))]]
     keyboardObj = ReplyKeyboardMarkup(keyboard)
     bot.sendMessage(chat_id=destinationChatId, text=_("hereYouHaveMenu"), reply_markup=keyboardObj)
 
-def rmMenu(destinationChatId):
+def rmMenu(destinationChatId): #Removes the in-keyboard menu if existing in specified destinationChatId
     bot.sendMessage(chat_id=destinationChatId, text=_("removingMenu"), reply_markup=ReplyKeyboardRemove())
 
-def sendFuckOff(destinationChatId):
+def sendFuckOff(destinationChatId): #Sends a message saying you shouldn't use this bot to the specified destinationChatId
     bot.sendMessage(chat_id=destinationChatId, text=_("thisIsPrivateBot"))
 
-def start(update, context):
+def start(update, context): #Start command. Presents itself and sends an in-keyboard menu
     if logCommand(update.effective_chat, "/start"):
         context.bot.sendMessage(chat_id=update.effective_chat.id, text=_("hello") + botName + _("useHelp"))
         sendMenu(update.effective_chat.id)
 
-def help(update, context):
+def help(update, context): #Help command. Tells what does each command
     if logCommand(update.effective_chat, "/help"):
         context.bot.sendMessage(chat_id=update.effective_chat.id, text=_("helpCmdListP1") + str(waitToCloseTime) + _("helpCmdListP2"))
 
-def openDoor(update, context):
+def openDoor(update, context): #Open command. Opens the door and closes after specified time
     if logCommand(update.effective_chat, "/open"): 
         context.bot.sendMessage(chat_id=update.effective_chat.id, text=_("openingDoor") + str(waitToCloseTime) + _("seconds") + _("."))
         openDoor()
 
-def toggle(update, context):
+def toggle(update, context): #Toggle command. Presses the button of the door only one time
     if logCommand(update.effective_chat, "/toggle"): 
         context.bot.sendMessage(chat_id=update.effective_chat.id, text=_("togglingDoor"))
         doorButton()
 
-def photo(update, context):
+def photo(update, context): #Photo command. Takes a photo from the piCamera and sends it
     if logCommand(update.effective_chat, "/photo"): 
         takePhoto()
         sendPhoto(update.effective_chat.id)
 
-def removemenu(update, context):
+def removemenu(update, context): #Removemenu command. Removes the in-keyboard menu from the sender's chatId
     if logCommand(update.effective_chat, "/removemenu"): 
         rmMenu(update.effective_chat.id)
 
-def sendmenu(update, context):
+def sendmenu(update, context): #Sendmenu command. Sends an in-keyboard menu to the sender's chatId
     if logCommand(update.effective_chat, "/sendmenu"): 
         sendMenu(update.effective_chat.id)
 
-def talk(update, context):
-    if logCommand(update.effective_chat, "Voice Note"):
+def talk(update, context): #Executed when someone sends a Voice Note. Plays the voice note on the speakers
+    if logCommand(update.effective_chat, _("voiceNote")):
         update.message.voice.get_file().download(custom_path="voice.mp3")
         sound = AudioSegment.from_file("voice.mp3")
         sound.export("voice.ogg", format="ogg") 
         playFile("voice.ogg")
 
+#Defining handlers
 startHandler = CommandHandler('start', start)  
 helpHandler = CommandHandler('help', help)
 openHandler = CommandHandler('open', openDoor)  
@@ -146,6 +150,7 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
+#Adding handlers
 dispatcher.add_handler(startHandler)
 dispatcher.add_handler(helpHandler)  
 dispatcher.add_handler(openHandler)  
